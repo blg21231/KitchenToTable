0c0d8999640e117e6757075777698f488763a188
blg21231 <39961911+blg21231@users.noreply.github.com>
2022-01-21 10:34:59 -0500

    Create Chef.py

---
+++ Chef.py
@@ -0,0 +1,8 @@
+chefs[chefs.user_id == user.id].pfp = user upload
+chefs[chefs.user_id == user.id].banner = user upload
+chefs[chefs.user_id == user.id].dishes = user upload
+ 
+ #User review
+ #if user chooses waiter:
+ #waiters.stars.append(user input)
+ #waiters.reviews.append(user input)
